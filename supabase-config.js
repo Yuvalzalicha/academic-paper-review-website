35de66001e0a1ff90fbb4aba8d3c858c4438f978
Yuval Zalicha <yuvalzalicha@Yuvals-MacBook-Pro.local>
2026-06-21 16:05:23 +0300

    Connect Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.PAPERTRAIL_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://iijjknxythzulznyisdt.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlpamprbnh5dGh6dWx6bnlpc2R0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMzQ5NDcsImV4cCI6MjA5NzYxMDk0N30.Wxh3WAlFGtl_zJjpJaZs7ZVnlSfxhnHx4XP28zSL2xY",
 };
